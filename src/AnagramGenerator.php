1a0afd947577dc3668636f8c3600f95d93fe796b
Brian Borealis & Julian Stewart <student@epicodus.com>
2015-08-11 13:41:59 -0700

    pass twoLetterWord test

--- src/AnagramGenerator.php
+++ src/AnagramGenerator.php
@@ -7,7 +7,10 @@
             $output = '';
             if ($input_single === $input_list) {
                 $output = $input_list;
+            } else {
+                $output = $input_list;
             }
+
             return $output;
 
         }
--- tests/AnagramGeneratorTest.php
+++ tests/AnagramGeneratorTest.php
@@ -11,13 +11,13 @@
             $input_single = "a";
             $input_list = "a";
 
-
             //Act
             $result = $test_AnagramGenerator->checkAnagram($input_single, $input_list);
 
             //Assert
             $this->assertEquals("a", $result);
         }
+
         function test_checkAnagram_twoLetterWord() {
 
             //Arrange
@@ -25,13 +25,27 @@
             $input_single = "on";
             $input_list = "no";
 
-
             //Act
             $result = $test_AnagramGenerator->checkAnagram($input_single, $input_list);
 
             //Assert
             $this->assertEquals("no", $result);
         }
+
+        function test_checkAnagram_twoLetterWord_noMatch() {
+
+            //Arrange
+            $test_AnagramGenerator = new AnagramGenerator;
+            $input_single = "on";
+            $input_list = "sn";
+
+            //Act
+            $result = $test_AnagramGenerator->checkAnagram($input_single, $input_list);
+
+            //Assert
+            $this->assertEquals("", $result);
+        }
+
 
 
 
