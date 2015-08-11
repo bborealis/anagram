e12bee84c16a9b001d79fdccd6b5cf13374e229e
Brian Borealis & Julian Stewart <student@epicodus.com>
2015-08-11 11:51:01 -0700

    update checkAnagram method.

--- src/AnagramGenerator.php
+++ src/AnagramGenerator.php
@@ -2,9 +2,14 @@
 
     class AnagramGenerator
     {
-        function checkAnagram($input_anagram)
+        function checkAnagram($input_single, $input_list)
         {
-            
+            $input_array_of_letters = explode("", $input_single);
+            $output_anagram = array();
+            $input_array_of_listLetters = explode("", $input_list);
+
+            for each
+            return sort($input_anagram);
         }
     }
 
--- tests/AnagramGeneratorTest.php
+++ tests/AnagramGeneratorTest.php
@@ -4,7 +4,20 @@
 
     class AnagramGeneratorTest extends PHPUnit_Framework_TestCase
     {
-        
+        function test_checkAnagram_twoLetterWord() {
+
+            //Arrange
+            $test_AnagramGenerator = new AnagramGenerator;
+            $input_single = "no";
+            $input_list = "on";
+
+
+            //Act
+            $result = $test_AnagramGenerator->checkAnagram($input_single, $input_list);
+
+            //Assert
+            $this->assertEquals("on", $result);
+        }
 
 
     }
